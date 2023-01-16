2f5e0bcbff63b1717a96a3e40e218e242ab03b44
benUsmane <syllaousmane279@gmail.com>
2023-01-16 15:14:45 +0000

    server

--- server/index.js
+++ server/index.js
@@ -1,8 +1,10 @@
 import express  from "express";
+import mongoose from "mongoose";
 import { router as courrierRouter } from "./routes/courrier.js";
 
 const app = express()
 app.use('/courrier',courrierRouter)
+mongoose.connect('mongodb+srv://BenUsmane:<python666920471usmane>@cluster0.ak7bh.mongodb.net/test',)
 app.listen(5572,()=>{
     console.log('app run well on port 5572')
 })
